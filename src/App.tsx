e871a56e80787ad4613e98786c0b07497d3af073
m2mt-l <ta28ma2@gmail.com>
2022-11-04 13:33:13 +0000

    add darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainPage from "./components/MainPage";
 
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 const App: React.FunctionComponent = () => {
-    return <MainPage />;
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? "dark" : "light",
+                },
+            }),
+        [prefersDarkMode],
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <MainPage />
+        </ThemeProvider>
+    );
 };
 
 export default App;
