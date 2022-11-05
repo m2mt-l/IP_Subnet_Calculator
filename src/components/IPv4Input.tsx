5ff598af7090a0573e710dcea3c97e1c89622cf1
m2mt-l <ta28ma2@gmail.com>
2022-11-05 00:31:09 +0000

    add ipv4State

--- src/components/IPv4Input.tsx
+++ src/components/IPv4Input.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -11,7 +11,16 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { generateIpv4Slash } from "../data/ipv4Subnet";
 
 const IPv4Input: FC = () => {
+    const [ipv4Address, setIpv4Address] = useState<string>("");
+
+    const handleIpv4AddressChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value } = event.target;
+        setIpv4Address(value);
+        // console.log(ipv4Address);
+    };
+
     const subnetString = generateIpv4Slash();
+
     return (
         <Stack spacing={2}>
             <TextField
@@ -19,13 +28,23 @@ const IPv4Input: FC = () => {
                 label="IPv4 address"
                 variant="filled"
                 sx={{ width: 300 }}
+                placeholder="192.0.2.1" // refactor
+                onChange={handleIpv4AddressChange}
             />
             <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Subnet</InputLabel>
-                <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Age">
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Age"
+                    defaultValue="24" // refactor
+                >
                     {subnetString.map((subnetString, index) => {
                         return (
-                            <MenuItem key={index} value={32 - index}>
+                            <MenuItem
+                                key={index}
+                                value={32 - index} // refactor
+                            >
                                 {subnetString}
                             </MenuItem>
                         );
