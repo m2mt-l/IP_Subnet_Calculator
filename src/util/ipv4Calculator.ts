70a83d0ae6db1f1b08289c5f020745caee938216
m2mt-l <ta28ma2@gmail.com>
2022-11-07 11:51:17 +0000

    add IP Type

--- src/util/ipv4Calculator.ts
+++ src/util/ipv4Calculator.ts
@@ -9,6 +9,7 @@ export function ipv4Calculator(type: string, ipv4Address: string, subnet: string
         [ipv4TypeKey.numberOfHosts]: getNumberOfHosts(subnet),
         [ipv4TypeKey.broadcastAddress]: getBroadcastAddress(ipv4Address, subnet),
         [ipv4TypeKey.subnetMask]: getSubnetMask(subnet),
+        [ipv4TypeKey.ipType]: getIPType(ipv4Address),
         [ipv4TypeKey.networkClass]: getNetworkClass(ipv4Address),
         [ipv4TypeKey.ipv4Mapped]: getIpv4MappedAddress(ipv4Address),
         [ipv4TypeKey.sixToFour]: getSixToFourAddress(ipv4Address),
@@ -24,6 +25,7 @@ Calculator functions
  -getNumberOfHosts
  -getBroadcastAddress
  -getSubnetMask
+ -getIPType
  -getNetworkClass
  -getIpv4MappedAddress
  -getSixToFourAddress
@@ -100,6 +102,24 @@ function getBroadcastAddress(ipv4Address: string, subnet: string): string {
 
 function getSubnetMask(subnet: string): string {
     return ipv4SubnetHashMap[subnet];
+}
+
+function getIPType(ipv4Address: string): string {
+    const ipv4AddressArray: number[] = splitIPv4Address(ipv4Address);
+    const firstOctet: number = ipv4AddressArray[0];
+    const SecondOctet: number = ipv4AddressArray[1];
+    let isPrivate = false;
+
+    // Private Class A
+    if (firstOctet === 10) isPrivate = true;
+    // Private Class B
+    else if (firstOctet === 172 && SecondOctet >= 16 && SecondOctet <= 31) isPrivate = true;
+    // Private Class C
+    else if (firstOctet === 192 && SecondOctet === 168) isPrivate = true;
+    // Global
+    else isPrivate = false;
+
+    return isPrivate ? "Private" : "Global";
 }
 
 function getIpv4MappedAddress(ipv4Address: string): string {
