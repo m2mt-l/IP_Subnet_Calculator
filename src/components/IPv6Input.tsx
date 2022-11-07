32ae1dc4966b41c574e372c6a5db6950296bb602
m2mt-l <ta28ma2@gmail.com>
2022-11-07 12:51:26 +0000

    add IPv6Input

--- src/components/IPv6Input.tsx
+++ src/components/IPv6Input.tsx
@@ -1,7 +1,39 @@
 import React, { FC } from "react";
 
+// Material UI
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+
 const IPv6Input: FC = () => {
-    return <div>IPv6Input</div>;
+    return (
+        <Stack spacing={2}>
+            <TextField
+                id="filled-basic"
+                label="IPv6 address"
+                variant="filled"
+                sx={{ minWidth: 370 }}
+                placeholder="test"
+            />
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Subnet</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Subnet"
+                    defaultValue="64"
+                ></Select>
+            </FormControl>
+            <Button variant="contained">Calculate</Button>
+            <Divider flexItem />
+        </Stack>
+    );
 };
 
 export default IPv6Input;
