c3d6ee46e1d50b3ef218aaf92b6d5dc53e3c92d7
m2mt-l <ta28ma2@gmail.com>
2022-11-08 13:30:46 +0000

    add ipv6ResultTable

---
+++ src/data/ipv6ResultTable.ts
@@ -0,0 +1,13 @@
+export const ipv6TypeKey: { [key: string]: string } = {
+    ipAddress: "IP address",
+    network: "Network",
+    ipAddressRange: "IP Address Range",
+    numberOfHosts: "Number of hosts",
+};
+
+export const ipv6ResultTable: string[] = [
+    ipv6TypeKey.ipAddress,
+    ipv6TypeKey.network,
+    ipv6TypeKey.ipAddressRange,
+    ipv6TypeKey.numberOfHosts,
+];
