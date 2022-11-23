5ee3d76c4dccc99e3490cf2f9dc955a306d65a0a
m2mt-l <ta28ma2@gmail.com>
2022-11-23 11:36:39 +0000

    feat: add handleIPv4AddressChange and handleSubnetChange

--- src/components/IPv4Summary.tsx
+++ src/components/IPv4Summary.tsx
@@ -18,16 +18,39 @@ import { generateIPv4Slash } from "../data/ipv4Subnet";
 import { isValidIpv4Address } from "../util/ipv4AddressValidation";
 import { DefaultIPv4 } from "../data/ipv4SubnetDefaultValue";
 import { IPv4Address } from "../model/IPv4Address";
-import InputIPv4AddressForSummary from "./uiParts/InputIPv4AddressForSummary";
-import SelectIPv4Subnet from "./uiParts/SelectIPv4Subnet";
 import CalculateButton from "./uiParts/CalculateButton";
 import AddAddressAndSubnetIcon from "./uiParts/AddAddressAndSubnetIcon";
 import RemoveAddressAndSubnetIcon from "./uiParts/RemoveAddressAndSubnetIcon";
 import { useAppContext } from "../contexts/AppContext";
+import { getIPv4SubnetValue } from "../util/ipv4Subnet";
 
 const IPv4Summary: FC = memo(function ipv4Summary() {
-    const { ipv4SummaryArray } = useAppContext();
-    // console.log(ipv4SummaryArray);
+    const subnetString = generateIPv4Slash();
+    const { addressAndSubnetForSummary, setAddressAndSubnetForSummary } = useAppContext();
+    const { ipv4SummaryArray, setIPv4SummaryArray } = useAppContext();
+
+    const handleIPv4AddressChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { value, id } = event.target;
+        setAddressAndSubnetForSummary({ ...addressAndSubnetForSummary, ipAddress: value });
+        setIPv4SummaryArray(
+            ipv4SummaryArray.map((value, index) =>
+                index === parseInt(id, 10) ? addressAndSubnetForSummary : value,
+            ),
+        );
+        // console.log(ipv4SummaryArray);
+    };
+
+    const handleSubnetChange = (event: SelectChangeEvent, selectedIndex: number): void => {
+        const { value } = event.target;
+        // console.log(selectedIndex);
+        // console.log(value);
+        setAddressAndSubnetForSummary({ ...addressAndSubnetForSummary, subnet: value });
+        setIPv4SummaryArray(
+            ipv4SummaryArray.map((value, index) =>
+                index === selectedIndex ? addressAndSubnetForSummary : value,
+            ),
+        );
+    };
 
     const renderAddressAndSubnet = ipv4SummaryArray.map((ipv4: IPv4Address, index: number) => (
         <Stack
@@ -43,8 +66,35 @@ const IPv4Summary: FC = memo(function ipv4Summary() {
             justifyContent="center"
             alignItems="center"
         >
-            <InputIPv4AddressForSummary />
-            <SelectIPv4Subnet />
+            {/* InputIPv4AddressForSummary */}
+            <TextField
+                id={index.toString()}
+                label="IPv4 address"
+                variant="filled"
+                sx={{ minWidth: 225 }}
+                placeholder={DefaultIPv4.placeholder}
+                onChange={handleIPv4AddressChange}
+            />
+            {/* SelectIPv4Subnet */}
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Subnet</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id={index.toString()}
+                    data-testid="subnet"
+                    label="Subnet"
+                    defaultValue={DefaultIPv4.subnet}
+                    onChange={(event) => handleSubnetChange(event, index)}
+                >
+                    {subnetString.map((subnetString, subnetIndex) => {
+                        return (
+                            <MenuItem key={subnetString} value={getIPv4SubnetValue(subnetIndex)}>
+                                {subnetString}
+                            </MenuItem>
+                        );
+                    })}
+                </Select>
+            </FormControl>
         </Stack>
     ));
 
--- src/components/uiParts/CalculateButton.tsx
+++ src/components/uiParts/CalculateButton.tsx
@@ -1,9 +1,21 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useEffect } from "react";
 
 import Button from "@mui/material/Button";
 
+import { useAppContext } from "../../contexts/AppContext";
+
 const CalculateButton: FC = memo(function calculateButton() {
-    return <Button variant="contained">Calculate</Button>;
+    const { ipv4SummaryArray, setIPv4SummaryArray } = useAppContext();
+
+    const handleCalculateClick = (): void => {
+        console.log(ipv4SummaryArray);
+    };
+
+    return (
+        <Button variant="contained" onClick={handleCalculateClick}>
+            Calculate
+        </Button>
+    );
 });
 
 export default CalculateButton;
