0b50fcd1e5ccb8fd1100eff571be4ea70a89636f
m2mt-l <ta28ma2@gmail.com>
2022-11-26 05:55:37 +0000

    feat: add context in add icon

--- src/components/uiParts/ipv6Summary/AddIPv6AddressIcon.tsx
+++ src/components/uiParts/ipv6Summary/AddIPv6AddressIcon.tsx
@@ -2,11 +2,30 @@ import AddIcon from "@mui/icons-material/Add";
 import IconButton from "@mui/material/IconButton";
 import React, { FC, memo } from "react";
 
+import { useAppContextForIPv6Summary } from "../../../contexts/AppContextForIPv6Summary";
 const AddIPv6AddressIcon: FC = memo(function addIPv6AddressIcon() {
+    const { ipv6SummaryArray, setIPv6SummaryArray } = useAppContextForIPv6Summary();
+
+    const addAddressAndSubnet = (): void => {
+        setIPv6SummaryArray([...ipv6SummaryArray, { ipAddress: "", subnet: "64", isShort: true }]);
+    };
+
+    const isMaximumIPv4SummaryArray = (): boolean => {
+        return ipv6SummaryArray.length >= 10;
+    };
+
     return (
-        <IconButton disabled>
-            <AddIcon />
-        </IconButton>
+        <div>
+            {isMaximumIPv4SummaryArray() ? (
+                <IconButton disabled>
+                    <AddIcon />
+                </IconButton>
+            ) : (
+                <IconButton onClick={addAddressAndSubnet}>
+                    <AddIcon />
+                </IconButton>
+            )}
+        </div>
     );
 });
 
