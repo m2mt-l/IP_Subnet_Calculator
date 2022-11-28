2553bcb5a5b03a2c9e5e44d94bd09fd2096887f6
m2mt-l <ta28ma2@gmail.com>
2022-11-28 05:25:02 +0000

    feat: add table in ResultIPv6Summary

--- src/components/ResultIPv6Summary.tsx
+++ src/components/ResultIPv6Summary.tsx
@@ -1,7 +1,24 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
 import React, { FC, memo } from "react";
 
 const ResultIPv6Summary: FC = memo(function resultIPv6Summary() {
-    return <div>ResultIPv6Summary</div>;
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 370, maxWidth: 500 }} aria-label="simple table">
+                <TableBody>
+                    <TableRow>
+                        <TableCell>Summary Address</TableCell>
+                        <TableCell align="right">IPv6 summary result</TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 });
 
 export default ResultIPv6Summary;
