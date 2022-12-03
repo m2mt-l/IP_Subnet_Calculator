ba730eb746b57d02d3626c460d839dff7f38dc8e
m2mt-l <ta28ma2@gmail.com>
2022-12-03 14:01:46 +0000

    fix: change max ip address summary

--- src/data/ipv4SummaryDefaultValue.ts
+++ src/data/ipv4SummaryDefaultValue.ts
@@ -2,6 +2,6 @@ export const defaultIPv4Address: number[] = [0, 0, 0, 0];
 
 export const DEFAULT_ROUTE = "0.0.0.0/0";
 
-export const MAX_IPV4_ADDRESS_SUMMARY = 10;
+export const MAX_IPV4_ADDRESS_SUMMARY = 5;
 
 export const MIN_IPV4_ADDRESS_SUMMARY = 2;
--- src/data/ipv6SummaryDefaultValue.ts
+++ src/data/ipv6SummaryDefaultValue.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_ROUTE = "::/0";
 
-export const MAX_IPV6_ADDRESS_SUMMARY = 10;
+export const MAX_IPV6_ADDRESS_SUMMARY = 5;
 
 export const MIN_IPV6_ADDRESS_SUMMARY = 2;
 
